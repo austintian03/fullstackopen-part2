72b5d9285ccabe848f04af266a2003d85b7f984d
Austin Tian <adt339@nyu.edu>
2024-03-25 18:29:09 -0400

    2.9 phonebook step4 - implement search field to filter list of people by name

--- phonebook/src/App.jsx
+++ phonebook/src/App.jsx
@@ -8,10 +8,15 @@ const Person = ({ person }) => {
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-1234567' }
-  ]) 
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
+  
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [searchTerm, setSearchTerm] = useState('')
 
   const addPerson = (event) => {
     event.preventDefault()
@@ -40,9 +45,22 @@ const App = () => {
     setNewNumber(event.target.value)
   }
 
+  const filterPeople = (event) => {
+    console.log(event.target.value)
+    setSearchTerm(event.target.value.toLowerCase()) 
+  }
+
+  const peopleToShow = (searchTerm === '') ? persons 
+    : persons.filter(person => person.name.toLowerCase().includes(searchTerm))
+  
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with <input value={searchTerm} onChange={filterPeople} />
+      </div>
+
+      <h2>add a new</h2>
       <form onSubmit={addPerson}>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
@@ -57,7 +75,7 @@ const App = () => {
 
       <h2>Numbers</h2> 
       <div>
-        {persons.map(person => 
+        {peopleToShow.map(person => 
             <Person key={person.name} person={person}/>
         )}
       </div>
